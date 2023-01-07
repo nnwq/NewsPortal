d273b7c861d26096fba4650bb5ef581446c8c7a1
Dmitry <dmitrypomishin@gmail.com>
2023-01-07 16:48:08 +0100

    Connected with a local db and completed first migration, removed the accounts app. Defined first models.

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -4,14 +4,30 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
+    def update_rating(self):
+        return
+
 
 class Category(models.Model):
 
 
 class Post(models.Model):
+    def like(self,):
+
+        return
+    def dislike(self,):
+        return
+
+    def preview(self,):
+        return
 
 
 class PostCategory(models.Model):
 
 
+
 class Comment(models.Model):
+    def like(self,):
+        return
+    def dislike(self,):
+        return
