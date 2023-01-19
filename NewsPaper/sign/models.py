04803aa3bff149140a11f71fd2d09c6a4cff29b5
Dmitry <dmitrypomishin@gmail.com>
2023-01-19 16:05:21 +0100

    final revision
    problem with permission required, was not able to implement it

--- NewsPaper/sign/models.py
+++ NewsPaper/sign/models.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
+
+
+class BasicSignupForm(SignupForm):
+
+    def save(self, request):
+        user = super(BasicSignupForm, self).save(request)
+        basic_group = Group.objects.get(name='common')
+        basic_group.user_set.add(user)
+        return user
 
 
 class BaseRegisterForm(UserCreationForm):
